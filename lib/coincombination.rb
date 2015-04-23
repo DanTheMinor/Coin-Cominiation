1b6edacaebae157f40bfc9550da1c7001aeb604d
Kim Dang & Dan Minor <student@epicodus.com>
2015-04-23 09:58:22 -0700

    Calculates coin combinations.

---
+++ lib/coincombination.rb
@@ -0,0 +1,29 @@
+require('pry')
+
+class Float
+  define_method(:coin_combination) do
+    quarters = 0
+    dimes = 0
+    nickels = 0
+    pennies = 0
+    total = self
+    until total.<=(0.24)
+      total = total.-(0.25)
+      quarters +=1
+    end
+    until total.<=(0.09)
+      total = total.-(0.10)
+      dimes +=1
+    end
+    until total.<=(0.04)
+    #  binding.pry
+      total = total.-(0.05)
+      nickels +=1
+    end
+    until total.<(0.0001)
+      total = total.-(0.01)
+      pennies +=1
+    end
+     quarters.to_s() + " quarters " + dimes.to_s() + " dimes " + nickels.to_s() + " nickels " + pennies.to_s() + " pennies"
+  end
+end
